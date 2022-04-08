a0fab85dc083de4e0bd7828dc988fe2217c29218
nawelfh <nawel.felah94@gmail.com>
2022-04-08 23:31:54 +0100

    adding darkMode [frontEnd]

--- cryptostats/src/App.tsx
+++ cryptostats/src/App.tsx
@@ -1,9 +1,19 @@
+import { createTheme , ThemeProvider } from '@mui/material';
 import React from 'react';
 import { LoginForm } from './components/auth/login-form/login-form.component';
 import "./index.css";
+
+const darkTheme = createTheme({
+  palette : {
+    mode :"dark",
+  },
+});
 function App() {
   return (
-    <LoginForm/>
+    <ThemeProvider theme = {darkTheme}>
+      <LoginForm/>
+    </ThemeProvider>
+    
   );
 }
 
