e726bc94069b18cf6f8b8ede614444e408a223c3
nawelfh <nawel.felah94@gmail.com>
2022-04-08 23:23:59 +0100

    fixing envirement and adding loginForm [FrontEnd]

---
+++ cryptostats/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LoginForm } from './components/auth/login-form/login-form.component';
+import "./index.css";
+function App() {
+  return (
+    <LoginForm/>
+  );
+}
+
+export default App;
---
+++ cryptostats/src/components/auth/login-form/login-form.component.tsx
@@ -0,0 +1,24 @@
+import { Button, TextField } from "@mui/material";
+import React from "react";
+
+
+const LoginForm : React.FC = () => {
+    return (
+        <div className = "flex justify-center items-center flex-col h-screen gap-8">
+            <h1 className="text-6xl">
+                CryptoStats
+            </h1>
+        <div className="flex flex-col gap-2">
+            <TextField label="Email" className="w-80" type="email" required/>
+            <TextField label="Password" className="w-80" type="password" required/>
+        </div>
+
+        <Button variant="contained" className="w-80">
+            <span className="p-1">Login</span>
+        </Button>
+
+        </div>
+    );
+};
+
+export {LoginForm}
